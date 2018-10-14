e15b809c286026f459c0a6850b9113e99f117a35
James Lafferty <james@j-lafferty.net>
2018-10-14 12:04:38 -0700

    Increase ship movement speed

--- ship.py
+++ ship.py
@@ -1,9 +1,11 @@
 import pygame
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position."""
         self.screen = screen
+        self.ai_settings = ai_settings
+
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/user_ship.bmp')
         self.rect = self.image.get_rect()
@@ -13,16 +15,23 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-        # Movement flags
+        # Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+
+        # Movement flags.
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flags."""
+        # Update the ship's center value, not the rect.
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center.
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship at its current location."""
