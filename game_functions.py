80dd09e13f79d4e2892f2664475ed9e0cbacd367
James Lafferty <james@j-lafferty.net>
2018-10-15 21:25:41 -0700

    Correct parameters updated in Bullet() call in def check_keydown_events()

--- game_functions.py
+++ game_functions.py
@@ -11,7 +11,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         # Create new bullet and add it to bullets group
-        new_bullet = Bullet(ai_settings, screen, bullets)
+        new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
