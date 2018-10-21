6fd565f853f7074e70f7d7df70a1a7fe701c3335
James Lafferty <james@j-lafferty.net>
2018-10-21 10:55:22 -0700

    Regenerate fleet once all aliens have been destroyed

--- game_functions.py
+++ game_functions.py
@@ -55,7 +55,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets, background):
     # Make the most recently drawn screen visible.
     pygame.display.flip()
 
-def update_bullets(aliens, bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """Update position of bullets and get rid of old bullets."""
     # Update bullet position.
     bullets.update()
@@ -70,6 +70,11 @@ def update_bullets(aliens, bullets):
     # Check for any bullets that have hit aliens.
     # If so, get rid of the bullet and the alien.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if len(aliens) == 0:
+        # Destroy existing bullets and create new fleet.
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
 
 def get_number_aliens_x(ai_settings, alien_width):
     """Determine the number of aliens that fit in a row."""
