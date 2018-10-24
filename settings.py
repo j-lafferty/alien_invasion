2d8c135dda3adc6556de384edcdde348dee8b10d
James Lafferty <james@j-lafferty.net>
2018-10-23 21:11:43 -0700

    Refactor to separate dynamic settings

--- settings.py
+++ settings.py
@@ -2,25 +2,35 @@ class Settings():
     """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the game's settings"""
+        """Initialize the game's static settings"""
         # Screen settings
         self.screen_width = 1100
         self.screen_height = 700
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed_factor = 3.5
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 4, 92, 160
         self.bullets_allowed = 3
 
         # Alien settings.
-        self.alien_speed_factor = 3
         self.fleet_drop_speed = 10
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
         # fleet direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
